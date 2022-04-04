0994fbac4fb94dd3ddeefa159c388b1112ab5d2e
Jan Kaczmarczyk <94491798+JasiekKaczmarczyk@users.noreply.github.com>
2022-04-04 21:57:17 +0200

    Add files via upload

---
+++ lab5/lab5.scala
@@ -0,0 +1,22 @@
+// Databricks notebook source
+// MAGIC %md
+// MAGIC ### Azure
+// MAGIC - Azure Machine Learning https://docs.microsoft.com/en-ca/azure/machine-learning/
+// MAGIC - Azure Data Factory https://docs.microsoft.com/en-ca/azure/data-factory/
+// MAGIC - Azure Data Lake Storage https://docs.microsoft.com/en-ca/azure/data-lake-store/
+// MAGIC - Azure Databricks https://docs.microsoft.com/en-us/azure/databricks/
+// MAGIC - Azure Synapse Analytics https://docs.microsoft.com/en-ca/azure/synapse-analytics/
+// MAGIC - Azure Stream Analytics https://docs.microsoft.com/en-ca/azure/stream-analytics/
+// MAGIC - Azure Data Share https://docs.microsoft.com/en-ca/azure/data-share/
+
+// COMMAND ----------
+
+// MAGIC %md
+// MAGIC ### AWS
+// MAGIC - Amazon Sagemaker https://docs.aws.amazon.com/sagemaker/
+// MAGIC - Amazon EMR https://docs.aws.amazon.com/emr/
+// MAGIC - Amazon Redshift https://docs.aws.amazon.com/redshift
+// MAGIC - Amazon CloudSearch https://docs.aws.amazon.com/cloudsearch/
+// MAGIC - Amazon Kinesis https://docs.aws.amazon.com/kinesis
+// MAGIC - AWS Glue https://docs.aws.amazon.com/glue
+// MAGIC - AWS Data Pipeline https://docs.aws.amazon.com/data-pipeline/
